12047e2341a537710e7f4e2ffe27aab27bd6456c
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-10-15 23:25:05 +0300

    ex64

---
+++ ex64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+//Ввод числа
+int GetNumber(string line)
+{
+Console.WriteLine(line);
+int number = int.Parse(Console.ReadLine() ?? "");
+return number;
+}
+
+// Вывод натуральных чисел от числа N до1
+void RecursionNumer(int n)
+{
+if (n < 1)
+return;
+Console.Write(n + ", ");
+RecursionNumer(n-1);
+//Console.Write(n + ", ");
+}
+
+//main
+int num = GetNumber("Введите число:");
+RecursionNumer(num);
